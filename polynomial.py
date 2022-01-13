1fe8e0f79b1118bb158c67523a5235cfdce96e85
Franck Donald Bassa <frk1990@hotmail.com>
2022-01-13 19:42:19 +0000

    Update polynomial.py

--- polynomial.py
+++ polynomial.py
@@ -4,18 +4,25 @@ import matplotlib.pyplot as plt  # type:ignore
 
 class Poly2:
     """ Classe permettant de representer un polynôme de degré 2."""
-
-    def __init__(self, *coeffs):
+    def __init__(self, coeffa, coeffb, coeffc):
         """ Méthode constructeur qui prend en paramètre, les coefficients du polynôme"""
-        pass
+        self.a = coeffa
+        self.b = coeffb
+        self.c = coeffc
 
     def __add__(self, other):
         """Addition 2 polynômes et qui renvoi du nouveau polynôme"""
-        pass
+        if isinstance(other, Poly2):
+            return Poly2(self.a + other.a, self.b + other.b, self.c + other.c)
+        else :
+            return Poly2(self.a, self.b, self.c + other)
 
     def __sub__(self, other):
         """Soustraction de 2 polynômes et renvoi du nouveau polynôme"""
-        pass
+        if isinstance(other, Poly2):
+            return Poly2(self.a - other.a, self.b - other.b, self.c - other.c)
+        else :
+            return Poly2(self.a, self.b, self.c - other)
 
     def __repr__(self):
         msg = 'Poly2(' + ', '.join([str(c) for c in sorted(self.coeffs.values())]) + ')'
@@ -26,11 +33,15 @@ class Poly2:
         Si: p1 = Poly(3, -4, 2)
         Alors print(p1) affiche: '2X^2 - 4X + 3'
         """
-        pass
+        s = "{0.a}X^2+{0.b}X+{0.c}".format(self)
+        return s
 
     def solve(self):
         """ Méthode qui renvoie les solutions si elles existent."""
-        pass
+        d = (self.b ** 2) - (4 * self.a * self.c)
+        res1 = (-self.b - sqrt(d)) / (2 * self.a)
+        res2 = (-self.b - sqrt(d)) / (2 * self.a)
+        return res1,res2
 
     def __val(self, x):
         """ Méthode qui calcule et renvoie la valeur de y en fonction de x.
@@ -38,11 +49,14 @@ class Poly2:
         Si: x prend pour valeur 5
         Alors: y = 5^2 + 1 = 26
         """
-        pass
+        val = self.a * x ** 2 + self.b * x + self.c
+        return val
 
     def draw(self, x_points=None):
         """ Méthode qui trace la courbe, voir fichier png."""
-        pass
+        polyCoeffiecients = [self.a,self.b,self.c]
+        plt.plot(polyCoeffiecients)
+        plt.show()
 
 
 if __name__ == "__main__":
